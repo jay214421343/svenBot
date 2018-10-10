4831e84e1ae19c4119defeb1b56334d74616d76c
caene <johan.e.h.lindell@gmail.com>
2018-10-10 23:59:57 +0200

    put youtube downloader options in the config file, seems fitting

--- docs/config.py
+++ docs/config.py
@@ -1,1 +1,17 @@
 token = "INSERT YOUR BOT TOKEN RIGHT HERE"
+
+ydl_opts = {
+    "default_search": "auto",
+    "format": "bestaudio/best",
+    "postprocessors": [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality': '192'}, {'key': 'FFmpegMetadata'}],
+    "extractaudio": True,
+    "nocheckcertificate": True,
+    "ignoreerrors": True,
+    "no_warnings": True,
+    "verbose": False,
+    "quiet": True,
+    "forcetitle": True,
+    "forceurl": True,
+    "skip_download": True,
+    "noplaylist": True
+    }
