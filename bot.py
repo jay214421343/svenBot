c43d523b870b625acbb64313c8e8988be30336d1
Caene <johan.e.h.lindell@gmail.com>
2019-01-16 01:23:14 +0100

    Cleaning up some code

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ song_volume = []
 # Initializes bot and prints out if the bot is ready/online.
 @client.event
 async def on_ready():
-    print("[status] svenBot is now online.")
+    print(f"[status] svenBot is now online.")
 
 # Removes the commands from Discord after being executed - clearing up all the clutter.
 @client.event
@@ -37,25 +37,25 @@ def check_queue(ctx):
         client.loop.create_task(client.send_message(ctx.message.channel, f"**Playing queued video:** {song_name[0]}"))
         print(f"[status] Playing queued video: {song_name[0]}")
         song_queue[0].start()
-    elif not song_queue:
+    else:
         server = ctx.message.server
         song_queue.clear()
         song_name.clear()
         song_volume.clear()
         voice_client = client.voice_client_in(server)
         voice_client.loop.create_task(voice_client.disconnect())
-        print("[status] Disconnected, no songs in queue..")
+        print(f"[status] Disconnected, no songs in queue..")
 
 # Will summon the bot and play or queue media.
 @client.command(pass_context=True)
 async def play(ctx, *, url):
     if "/playlist" in url:
-        await client.say("You can't queue playlists..")
+        await client.say(f"You can't queue playlists..")
     else:
         server = ctx.message.server
         if client.is_voice_connected(server):
             voice_client = client.voice_client_in(server)
-        elif not client.is_voice_connected(server):
+        else:
             channel = ctx.message.author.voice.voice_channel
             await client.join_voice_channel(channel)
             voice_client = client.voice_client_in(server)
@@ -65,8 +65,8 @@ async def play(ctx, *, url):
             song_queue.append(player)
             song_name.append(player.title)
             print(f"[status] Queuing: {player.title}")
-            await client.say("**Queuing video..**")
-        elif not song_queue:
+            await client.say(f"**Queuing video..**")
+        else:
             song_queue.append(player)
             song_name.append(player.title)
             player.start()
@@ -77,9 +77,9 @@ async def play(ctx, *, url):
 # Outputs the current queue.
 @client.command(pass_context=True)
 async def queue(ctx):
-    await client.say("__**CURRENT QUEUE:**__")
+    await client.say(f"__**CURRENT QUEUE:**__")
     for number, song in enumerate(song_name, 1):
-        await client.say("%d: %s" % (number, song))
+        await client.say(f"{number}: {song}")
 
 # Volume control.
 @client.command(pass_context=True)
@@ -92,34 +92,34 @@ async def vol(ctx, value: int):
             song_queue[0].volume = value / 100
             song_volume.append(song_queue[0].volume)
             await client.say(f"**Volume set to:** {str(value)}%")
-    elif not song_queue:
-        await client.say("**There's nothing playing, can't adjust volume..**")
+    else:
+        await client.say(f"**There's nothing playing, can't adjust volume..**")
 
 # Outputs current volume.
 @client.command(pass_context=True)
 async def currentvol(ctx):
     if song_queue:
         await client.say(f"**Current volume:** {int(song_volume[0] * 100)}%")
-    elif not song_queue:
-        await client.say("**There's nothing playing, no current volume..**")
+    else:
+        await client.say(f"**There's nothing playing, no current volume..**")
 
 # Resumes paused player.
 @client.command(pass_context=True)
 async def resume(ctx):
     if song_queue:
         song_queue[0].resume()
-        await client.say("**Resuming video..**")
-    elif not song_queue:
-        await client.say("**There's nothing to resume..**")
+        await client.say(f"**Resuming video..**")
+    else:
+        await client.say(f"**There's nothing to resume..**")
 
 # Pauses player.
 @client.command(pass_context=True)
 async def pause(ctx):
     if song_queue:
         song_queue[0].pause()
-        await client.say("**Pausing video..**")
-    elif not song_queue:
-        await client.say("**There's nothing to pause..**")
+        await client.say(f"**Pausing video..**")
+    else:
+        await client.say(f"**There's nothing to pause..**")
 
 # Makes bot leave the current voice channel.
 @client.command(pass_context=True)
@@ -131,9 +131,9 @@ async def leave(ctx):
         song_name.clear()
         song_volume.clear()
         await voice_client.disconnect()
-        print("[status] Cleared queue and disconnected..")
-    elif not client.voice_client_in(server):
-        await client.say("**Can't leave if I'm not in a voice channel..**")
+        print(f"[status] Cleared queue and disconnected..")
+    else:
+        await client.say(f"**Can't leave if I'm not in a voice channel..**")
 
 # Skips current song to the next song in queue.
 @client.command(pass_context=True)
@@ -141,9 +141,9 @@ async def skip(ctx):
     if song_queue:
         song_queue[0].pause()
         check_queue(ctx)
-        await client.say("**Skipping video..**")
-    elif not song_queue:
-        await client.say("**There's nothing to skip..**")
+        await client.say(f"**Skipping video..**")
+    else:
+        await client.say(f"**There's nothing to skip..**")
 
 # Outputs current weather in given area.
 @client.command(pass_context=True)
@@ -179,6 +179,6 @@ async def botcommands(ctx):
         "**!weather:** Input a city name to get weather info",
         "**!forecast:** Input a city name to get forecast info"
     ]
-    await client.say("\n".join(botcommands_list))
+    await client.say(f"\n".join(botcommands_list))
 
 client.run(config.token)
